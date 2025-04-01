7dd6600c2cb74efb70952ec14235e1ce45c1f770
Hea Sonetra <heasonetra27@gmail.com>
2025-04-01 22:52:28 +0700

    last update

--- lib/model/ProductModel.dart
+++ lib/model/ProductModel.dart
@@ -6,10 +6,12 @@ class Productmodel {
   final String title;
   final double price;
   final String img;
-  final List preview;
+  final List size;
   final double map;
   final int counter;
   final double calories;
+  final String decription;
+  final double rate;
   final int CategoryID;
 
   Productmodel({
@@ -17,57 +19,85 @@ class Productmodel {
     required this.title,
     required this.price,
     required this.img,
-    required this.preview,
+    required this.size,
     required this.map,
     required this.counter,
     required this.calories,
+    required this.decription,
+    required this.rate,
     required this.CategoryID,
   });
 }
 
 List<Productmodel> listProduct = [
+  // Productmodel(
+  //   id: 1,
+  //   title: 'Metting chhese pizza',
+  //   price: 10,
+  //   img: 'assets/pizza1.png',
+  //   preview: [],
+  //   map: 20,
+  //   counter: 0,
+  //   calories: 40,
+  //   CategoryID: listCategory[0].id,
+  // ),
+  // Productmodel(
+  //   id: 2,
+  //   title: 'Shusi for you',
+  //   price: 10,
+  //   img: 'assets/shusi.png',
+  //   preview: [],
+  //   map: 20,
+  //   counter: 0,
+  //   calories: 50,
+  //   CategoryID: listCategory[0].id,
+  // ),
+  // Productmodel(
+  //   id: 3,
+  //   title: 'Lemom cat tea',
+  //   price: 10,
+  //   img:'assets/drink.jpg',
+  //   preview: [],
+  //   map: 20,
+  //   counter: 0,
+  //   calories: 30,
+  //   CategoryID: listCategory[1].id,
+  // ),
+  // Productmodel(
+  //   id: 4,
+  //   title: 'Berger a title calories',
+  //   price: 10,
+  //   img:'assets/berger.png',
+  //   preview: [],
+  //   map: 20,
+  //   counter: 0,
+  //   calories: 10,
+  //   CategoryID: listCategory[0].id,
+  // ),
   Productmodel(
     id: 1,
     title: 'Metting chhese pizza',
     price: 10,
     img: 'assets/pizza1.png',
-    preview: [],
-    map: 20,
+    size: ['Small','Mediem'],
+    map: 2.4,
     counter: 0,
-    calories: 40,
+    calories: 4,
+    decription: "Pizza out fit low calories",
+    rate: 4.9,
     CategoryID: listCategory[0].id,
   ),
   Productmodel(
     id: 2,
-    title: 'Shusi for you',
-    price: 10,
+    title: 'Shusi',
+    price: 8,
     img: 'assets/shusi.png',
-    preview: [],
-    map: 20,
+    size: ['Small','Mediem','Big'],
+    map: 2.4,
     counter: 0,
-    calories: 50,
-    CategoryID: listCategory[0].id,
-  ),
-  Productmodel(
-    id: 3,
-    title: 'Lemom cat tea',
-    price: 10,
-    img:'assets/drink.jpg',
-    preview: [],
-    map: 20,
-    counter: 0,
-    calories: 30,
+    calories: 4,
+    decription: "Pizza out fit low calories",
+    rate: 4.9,
     CategoryID: listCategory[1].id,
-  ),
-  Productmodel(
-    id: 4,
-    title: 'Berger a title calories',
-    price: 10,
-    img:'assets/berger.png',
-    preview: [],
-    map: 20,
-    counter: 0,
-    calories: 10,
-    CategoryID: listCategory[0].id,
   ),
 ];
--- lib/view/detailpage.dart
+++ lib/view/detailpage.dart
@@ -9,48 +9,128 @@ class Detailpage extends StatefulWidget {
   State<Detailpage> createState() => _DetailpageState();
 }
 
+var check = false;
+
 class _DetailpageState extends State<Detailpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: EdgeInsets.all(20),
-       child: Column(
-        children: [
-          SizedBox(height: 30,),
-          Row(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(Icons.arrow_back)),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(Icons.arrow_back),
+                ),
                 Spacer(),
-                Icon(Icons.favorite_border_outlined),
-                
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      check = !check;
+                    });
+                  },
+                  child:
+                      (check == false)
+                          ? Icon(Icons.favorite_border_outlined)
+                          : Icon(Icons.favorite, color: Colors.red),
+                ),
+
                 Padding(
-                  padding: const EdgeInsets.only(left: 20,right: 14),
+                  padding: const EdgeInsets.only(left: 20, right: 14),
                   child: Icon(Icons.share),
-                )
-            ],
-          ),
-          SizedBox(height: 20,),
-          Container(
-            height: 240,
-            width: 240,
-            decoration: BoxDecoration(
-              image: DecorationImage( 
-                image:AssetImage(widget.productmodel.img),)
+                ),
+              ],
             ),
-          ),
-          SizedBox(height: 20,),
-          Row(
-            children: [
-              Text(widget.productmodel.title,style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
-            ],
+            SizedBox(height: 20),
+            Container(
+              height: 240,
+              width: 240,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(widget.productmodel.img),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Text(
+                  widget.productmodel.title,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Icon(Icons.food_bank,color: Colors.red.withOpacity(0.5),),
+                SizedBox(width: 8),
+                Text(widget.productmodel.decription,style: TextStyle(fontSize: 14,color: Colors.grey[600]),),
+                SizedBox(width: 30),
+                Icon(Icons.star,size: 26,color: Colors.green,),
+                SizedBox(width: 5),
+                Text('${widget.productmodel.rate}',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
+                SizedBox(width: 5),
+                Text('(${widget.productmodel.map}) Km',style: TextStyle(fontSize: 14,color: Colors.grey[600]),),
+                SizedBox(width: 5),
+                Icon(Icons.arrow_forward_ios_outlined,size: 12,color: Colors.grey,)
+              ],
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: double.infinity,
+              height: 120,
+              decoration: BoxDecoration(
+                color: Colors.amber
+              ),
+              child: Row(
+              children: [
+                // GridView.count(
+                //   scrollDirection: Axis.horizontal,
+                //   crossAxisCount: 3,
+                //   mainAxisSpacing: 10,
+                //   crossAxisSpacing: 10,
+                //   childAspectRatio: 1/1,
+                //   children: List.generate(widget.productmodel.size.length,(index)=>Container(
+                //       height: 100,
+                //       decoration: BoxDecoration(
+                        
+                //         color: const Color.fromARGB(255, 24, 18, 216)
+                //       ),
+                //     )
+                //   )
+                // ),
+                Expanded(
+                  child: GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    mainAxisSpacing: 10,
+                   
+                    ), 
+                    itemCount: widget.productmodel.size.length,
+                    itemBuilder: (context,index){
+                    return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue
+                          ),
+                    );
+                  }),
+                )
+              ],
+            ),
           )
-        ],
-       ),
-       ),
+            
+          ],
+        ),
+      ),
     );
   }
 }
